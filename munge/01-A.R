f5c90722e32adc5c0ce3ebdacd81d93eab4460d0
Michael Ching <mikeching@yahoo.com>
2013-05-31 08:28:06 -1000

    Rename dataframe to data for easier referencing

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,8 @@
-# Example preprocessing script.
+##################################
+# Munge file for NSCH project
+##################################
+
+# Rename data file for easier referencing
+data <- DRC.2011.2012.NSCH
+rm(DRC.2011.2012.NSCH)
+
