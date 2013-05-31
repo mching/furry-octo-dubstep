c1bd203af1a486fa70fa56cbff7dedbf64e2a7d4
Michael Ching <mikeching@yahoo.com>
2013-05-31 09:38:21 -1000

    Add survey design to end of munge file

--- munge/01-A.R
+++ munge/01-A.R
@@ -6,3 +6,9 @@
 data <- DRC.2011.2012.NSCH
 rm(DRC.2011.2012.NSCH)
 
+# Any variables that need to be cleaned follow this line:
+
+
+# Specify survey design
+nsch.design <- svydesign(id=~IDNUMR, strata = ~STATE + SAMPLE, data = data, weights = ~NSCHWT)
+# nsch.design
